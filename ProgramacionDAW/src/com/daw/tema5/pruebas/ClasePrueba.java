92e69babe0c7269575b1bb9507490241da13e886
Lainezz <diegolinaresmojacar@gmail.com>
2021-11-04 18:55:57 +0100

    Commit con cambios en la ClasePrueba
    Signed-off-by: Lainezz <diegolinaresmojacar@gmail.com>

--- ProgramacionDAW/src/com/daw/tema5/pruebas/ClasePrueba.java
+++ ProgramacionDAW/src/com/daw/tema5/pruebas/ClasePrueba.java
@@ -5,88 +5,103 @@ import java.util.Scanner;
 
 public class ClasePrueba {
 	public static void main(String[] args) {
-		/*
+		
 		//Colecciones
 		
 		//Los arraylists
+		
+		//TIPOS DE DATOS QUE ADMITEN LOS ARRAYLISTS (hay más)
+		//Integer = int
+		//Double = double
+		//String = String
+		//Boolean = boolean
+		//Character = char
+		
+		//ArrayList<tipo de dato que se va a almacenar> nombreVariable = new ArrayList<tipo de dato>();
+		//Un arraylist de enteros
 		ArrayList<Integer> arr = new ArrayList<Integer>();
-		
-		//Inserta un elemento en la lista (al final)
-		arr.add(1);
-		//Inserta un elemento en la lista en una posición dada (posicion, valor)
-		arr.add(0, 4);
-		
-		System.out.println(arr);
-		
-		
-		ArrayList<Character> arrC = new ArrayList<Character>();
-		arrC.add('H');
-		arrC.add('O');
-		arrC.add('L');
-		arrC.add('A');
-		
-		System.out.println(arrC);
-		*/
-		
-		Scanner scan = new Scanner(System.in);
-		
-		System.out.println("Introduzca el primer número: ");
-		int num1 = scan.nextInt();
-		
-		System.out.println("Gracias, introduzca a continuación el siguiente número: ");
-		int num2 = scan.nextInt();
-		
-		int suma = sumar(num1, num2);
-		System.out.println("El resultado de la suma sería: "+suma);
+		//Un arraylist de double
+		ArrayList<Double> arrDoub = new ArrayList<Double>();
+		//Un arraylist de Strings donde se van a almacenar los alumnos de una clase
+		ArrayList<String> arrStr = new ArrayList<String>();
+		//Un arraylist de booleanos
+		ArrayList<Boolean> arrBool = new ArrayList<Boolean>();
 		
 		
-		int num3;
-		int num4;
+		//A mi array de String le voy a añadir nuevos alumnos
+		arrStr.add("Luis");
+		arrStr.add("Fran");
+		arrStr.add("Sebas");
+		arrStr.add("Antonio");
+		arrStr.add("Carlos");
+		arrStr.add("Roberto");
+		arrStr.add("Alba");
+		arrStr.add("Joseph");
+		arrStr.add("Miguel Angel");
+		arrStr.add("Conrado");
 		
-		System.out.println("Por favor, introduzca el primer número: ");
-		num3 = scan.nextInt();
+		System.out.println("Alumnos 1º: "+arrStr);  //Mostramos por pantalla el array list
 		
-		System.out.println("Introduzca el segundo número: ");
-		num4 = scan.nextInt();
+		arrStr.add("Mbappé"); //Añado al alumno Mbappé
 		
-		int mayor = mayorQue(num3, num4);
+		System.out.println("Alumnos 1º: "+arrStr);  //Mostramos por pantalla el array list
 		
-		System.out.println("El numero mayor es "+mayor);
+		arrStr.add("CR7"); //Añado al alumno CR7
+		System.out.println("Alumnos 1º: "+arrStr);  //Mostramos por pantalla el array list
 		
-		/*
-		1º Programa que lea por teclado 3 números y que muestre por pantalla el mayor número
-		de esos 3.
-			a) Hacer un método que reciba 3 variables int por parámetro y que devuelva la suma de los 3
-			b) Hacer un método que reciba 3 variables int por parámetro y que devuelva el número
-				mayor de esos 3 números.
+		//Devuelve la cantidad de elementos que tiene la colección
+		int tamano = arrStr.size();
+		System.out.println("El tamaño es: "+tamano); //Imprimo por pantalla el tamaño de mi arraylist
 		
-		2º Programa que lea por teclado 5 números y que los almacene dentro de un array de enteros.
-		El programa muestra la suma de todos los números introducidos.
-			a) Hacer un método que reciba un array de enteros y devuelva la suma de los elementos de
-			ese array.
+		//Elimina todos los elementos de una colección
+		arrStr.clear();
 		
+		//Mostramos por pantalla el tamaño de nuestro arraylist
+		System.out.println("El tamano es: "+arrStr.size());
 		
-		3º Buscar por internet cómo convertir un String a un array de caracteres.
-			a) Hacer un método que reciba un String por parámetros y devuelva un array de caracteres.
+		//Mostramos por pantalla el array list
+		System.out.println("Alumnos 1º: "+arrStr);
 		
+		//Insertamos elementos en el arraylist
+		arrStr.add("Luis");
+		arrStr.add("Fran");
+		arrStr.add("Sebas");
+		arrStr.add("Antonio");
+		arrStr.add("Carlos");
+		arrStr.add("Roberto");
+		arrStr.add("Alba");
+		arrStr.add("Joseph");
+		arrStr.add("Miguel Angel");
+		arrStr.add("Conrado");
+		System.out.println("Alumnos 1º: "+arrStr); //Mostramos por pantalla el array list
 		
-		4º Buscar información de varios métodos de la clase String ¿Qué hacen? ¿Qué reciben por parámetros? ¿Qué devuelven?
-			Conrado y M.A
-			a) .charAt() y .equals()
-			Joseph Alba
-			a) .contains() .replaceAll()
-			Luis Fran Sebas
-			a) .toLowerCase() .concat() .equalsIgnoreCase()
-			Antonio Carlos Roberto
-			a) .toUpperCase() .indexOf() .lastIndexOf()
+		//Eliminamos lo que contiene la posición 7 del arraylist
+		System.out.println("Remove");
+		arrStr.remove(7);
 		
-		 */
-		
-		
-		
-		
+		System.out.println("Alumnos 1º: "+arrStr);  //Mostramos por pantalla el array list
 		
 		
+		//Eliminamos algún elemento del arraylist
+		System.out.println("Remove a Antonio");
+		arrStr.remove("Antonio");
+		
+		System.out.println("Alumnos 1º: "+arrStr);  //Mostramos por pantalla el array list
+		
+		
+		//Index of -> te devuelve la posición de un elemento que nosotros le indicamos
+		int posicion = arrStr.indexOf("Fran");
+		
+		System.out.println("La posición de Fran es: "+posicion); //Imprimo por pantalla la posición del elemento dado
+		
+		//Last index of -> te devuelve la última posición de un elemento que nosotros le indicamos.
+		arrStr.add("Fran"); //Añado un nuevo alumno
+		System.out.println("Alumnos 1º: "+arrStr); //Mostramos por pantalla el array list
+		System.out.println("La última posición de Fran es: "+arrStr.lastIndexOf("Fran")); //Muestro por pantalla la última posición de un elemento que nosotros le indicamos
+		
+		
+		//Llamo a mi método sumatoria
+		sumatoria(10);
 		
 		
 	}
@@ -159,24 +174,41 @@ public class ClasePrueba {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	/**
+	 * Método que calcula la suma de todos los elementos de un arraylist
+	 * @param num
+	 */
+	public static void sumatoria(int num) {
+		
+		Scanner scan = new Scanner(System.in);
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		int numeroParaIntroducir = 0;
+		
+		//Con este for tendría un arraylist relleno de todos los números que quisiera
+		for(int i=0; i<num; i++) {
+			System.out.println("Introduzca un número al array"); //muestro un mensaje al usuario
+			numeroParaIntroducir = scan.nextInt(); //leo por teclado lo que el usuario nos ha dicho
+			arr.add(numeroParaIntroducir); //Añado el elemento a mi arraylist
+		}
+		
+		//Con este for realizaría la sumatoria de los elementos
+		int sumatoria = 0;
+		for(int i=0; i<arr.size(); i++) {
+			sumatoria += arr.get(i); //Obtengo lo que contiene la posición i del arraylist, y lo sumo
+		}
+		
+		System.out.println("La sumatoria es: "+sumatoria);
+		
+		/*
+		CREAR UNA LISTA CON LAS LETRAS DEL ALFABETO
+		MOSTRAR LA LISTA
+		BUSCAR Y MOSTRAR LA POSICIÓN DE ‘E’, ‘H’ Y ‘O’
+		ELIMINAR ELEMENTO EN LA POSICIÓN 1 Y ‘U’
+		MOSTRAR LA LISTA DE NUEVO.
+		*/
+		
+		
+	}
 	
 	
 	
