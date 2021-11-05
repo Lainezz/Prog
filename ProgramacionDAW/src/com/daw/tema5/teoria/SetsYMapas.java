73b22d240c7be368fc879e9c24f1018cf75dc813
Lainezz <diegolinaresmojacar@gmail.com>
2021-11-05 19:05:32 +0100

    Clase 05 Nov
    Signed-off-by: Lainezz <diegolinaresmojacar@gmail.com>

--- ProgramacionDAW/src/com/daw/tema5/teoria/SetsYMapas.java
+++ ProgramacionDAW/src/com/daw/tema5/teoria/SetsYMapas.java
@@ -6,13 +6,16 @@ public class SetsYMapas {
 	
 	public static void main(String[] args) {
 		System.out.println("SETS Y MAPS\n");
+		
 		//SETS
 		//Tipo de colección donde no puede hacer elementos repetidos
 		//
 		//Hashset. Colección que implementa tabla Hash.
+		//Para declarar un hashSet
+		//Interfaz(Set)<Tipo de dato> nombre = new HashSet<Tipo de dato>();
 		Set<String> setHash = new HashSet<String>();
 		
-		//Para anadir elementos, hacemos igual que con el Arraylist
+		//Para añadir elementos, hacemos igual que con el Arraylist
 		setHash.add("Hola");
 		setHash.add("Maniana");
 		setHash.add("Diego");
@@ -22,9 +25,11 @@ public class SetsYMapas {
 		//Al imprimir nuestra colección, vemos que solo hay 1 elemento con valor "Diego", por la condición de los SETS de que no pueden haber elementos repetidos
 		System.out.println("SetHash creado:\n"+setHash);
 		
+		
 		//¿Como hace para no insertar dos elementos iguales?
 		String cadena = "Diego";
-		System.out.println("\nLa cadena "+cadena + " tiene un hashCode:"+ cadena.hashCode()+"\n");
+		System.out.println("\nLa cadena "+ cadena + " tiene un hashCode:"+ cadena.hashCode()+"\n");
+		
 		
 		for(String s : setHash) {
 			System.out.println(s + " tiene un hashCode: "+ s.hashCode());
@@ -32,25 +37,32 @@ public class SetsYMapas {
 
 		//
 		//TreeSet. Colección que almacena sus elementos ordenados
+		//Interfaz(Set)<Tipo de dato> nombre = new TreeSet<Tipo de dato>();
 		Set<String> treeSet = new TreeSet<String>();
 		
-		//Para anadir elementos, hacemos igual que con el Arraylist
+		//Para anadir elementos, hacemos igual que con el Arraylist e igual que el HashSet
 		treeSet.add("Diego");
 		treeSet.add("Biego");
 		treeSet.add("Biego");
+		treeSet.add("Bingo");
+		treeSet.add("Zuzo");
+		treeSet.add("Helicoptero");
 		
 		//Vemos que inserta los elementos de manera ordenada (en este caso, de forma alfabética)
 		System.out.println("\nTreeSet creado:\n"+treeSet);
+		
 		
 		//MAPS
 		//Admite dos valores por elemento y no puede haber elementos repetidos
 		//
 		//HashMap. Coleccion que implementa tabla hash
+		//Interfaz<tipo de dato de clave, tipo de dato de valor> nombre = new HashMap<tipo de dato de clave, tipo de dato de valor>();
 		Map<String, String> hashMap = new HashMap<String, String>();
+		
 		hashMap.put("75186773B", "Diego"); 	//Insertamos clave - valor
 		hashMap.put("75186773B","Alfonso"); //La clave ya existe... no añade una clave nueva, si no que sobreescribe el valor
 		hashMap.put("64075662A", "Pepito"); //La clave no existe... se añade de forma normal sin orden específico
-		hashMap.put("64075662B", "Diego");	//La clave no existe... se añade de forma normal sin orden específico
+		hashMap.put("64075662B", "Ruben");	//La clave no existe... se añade de forma normal sin orden específico
 		
 		System.out.println("\nHashMap creado:\n"+hashMap); //Como vemos, los elementos se han ido añadiendo sin un orden específico
 		
@@ -84,15 +96,46 @@ public class SetsYMapas {
 		
 		//
 		//TreeMap. Coleccion que inserta sus elementos ordenandos por su clave
+		//Interfaz<Tipo dato clave, tipo dato valor> nombre = new TreeMap<tipo dato clave, tipo dato valor>();
 		Map<Integer, String> treeMapInt = new TreeMap<Integer, String>();
-		treeMapInt.put(4, "Linares");
+		treeMapInt.put(1, "Linares");
 		treeMapInt.put(2, "Aguilar");
 		treeMapInt.put(7, "Fernandez");
-		treeMapInt.put(1, "Castañeda");
+		treeMapInt.put(9, "Castañeda");
 		treeMapInt.put(5, "Gutierrez");
 		treeMapInt.put(6, "Protasio");
 		treeMapInt.put(3, "Belisario");
 		System.out.println("\nTreeMap con nombres asociados a una clave:\n"+treeMapInt);
 		
+		//
+		//Iteradores
+		//Podemos recorrer las colecciones con un foreach
+		//for ( tipo_variable nombre_variable : la lista por la que queremos iterar)
+		for (String cad : setHash) {
+			System.out.println("La cadena es: "+cad);
+		}
+		
+		Set<Integer> setHashInt = new HashSet<Integer>();
+		setHashInt.add(10);
+		setHashInt.add(20);
+		setHashInt.add(30);
+		setHashInt.add(40);
+		System.out.println(setHashInt);
+		
+		for (int numero : setHashInt) {
+			System.out.println("El numero es: "+numero);
+		}
+		
+		
+		/*
+		 * 1. Crear una colección por cada tipo de colección. (Arraylist) (Set -> HashSet y TreeSet) (Maps -> HashMap y TreeMap)
+		 * 2. Meter valores a cada una de las colecciones creadas
+		 * 		a) En el caso de HashMap y TreeMap... esos valores tienen que tener sentido
+		 * 3. Recorrer con un foreach cada una de las colecciones Set y Arraylist que habéis creado.
+		 */
+		
+		
+
 	}
+
 }
