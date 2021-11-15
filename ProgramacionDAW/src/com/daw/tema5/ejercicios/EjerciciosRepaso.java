4577eaf8ab8dd7200bd591ecfe67bd80d41fb603
Lainezz <diegolinaresmojacar@gmail.com>
2021-11-15 12:00:50 +0100

    Modificasión de cosas
    Signed-off-by: Lainezz <diegolinaresmojacar@gmail.com>

---
+++ ProgramacionDAW/src/com/daw/tema5/ejercicios/EjerciciosRepaso.java
@@ -0,0 +1,14 @@
+package com.daw.tema5.ejercicios;
+
+public class EjerciciosRepaso {
+	public static void main(String[] args) {
+		
+		// Sets y Maps
+		/*
+		 * 1. Crear una colección por cada tipo de colección. (Arraylist) (Set -> HashSet y TreeSet) (Maps -> HashMap y TreeMap)
+		 * 2. Meter valores a cada una de las colecciones creadas
+		 * 		a) En el caso de HashMap y TreeMap... esos valores tienen que tener sentido
+		 * 3. Recorrer con un foreach cada una de las colecciones que habéis creado.
+		 */
+	}
+}
--- ProgramacionDAW/src/com/daw/tema5/teoria/SetsYMapas.java
+++ ProgramacionDAW/src/com/daw/tema5/teoria/SetsYMapas.java
@@ -138,20 +138,6 @@ public class SetsYMapas {
 			entry.getKey();
 			entry.getValue();
 		}
-		
-		
-		/*
-		 * 1. Crear una colección por cada tipo de colección. (Arraylist) (Set -> HashSet y TreeSet) (Maps -> HashMap y TreeMap)
-		 * 2. Meter valores a cada una de las colecciones creadas
-		 * 		a) En el caso de HashMap y TreeMap... esos valores tienen que tener sentido
-		 * 3. Recorrer con un foreach cada una de las colecciones que habéis creado.
-		 */
-		
-		
-		
-		
-		
-
 	}
 
 }
